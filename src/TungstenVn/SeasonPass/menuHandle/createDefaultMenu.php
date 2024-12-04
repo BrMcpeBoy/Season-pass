7f59a7318c65477a97260a3e73f1dd58619cb687
BrMcpeBoy <68336057+BrMcpeBoy@users.noreply.github.com>
2024-12-04 23:22:04 +0700

    Update createDefaultMenu.php

--- src/TungstenVn/SeasonPass/menuHandle/createDefaultMenu.php
+++ src/TungstenVn/SeasonPass/menuHandle/createDefaultMenu.php
@@ -2,10 +2,7 @@
 
 namespace TungstenVn\SeasonPass\menuHandle;
 
-use pocketmine\block\utils\DyeColor;
-use pocketmine\block\VanillaBlocks;
-use pocketmine\item\ItemFactory;
-use pocketmine\item\VanillaItems;
+use pocketmine\item\{Item, StringToItemParser, LegacyStringToItemParser};
 use TungstenVn\SeasonPass\menuHandle\menuHandle;
 use muqsit\invmenu\InvMenu;
 use muqsit\invmenu\type\InvMenuTypeIds;
@@ -23,26 +20,26 @@ class createDefaultMenu
     {
         $menu = InvMenu::create(InvMenuTypeIds::TYPE_DOUBLE_CHEST);
         $menu->setName("§lSSP MÙA I §r(XEM CẤP /minelevel)");
-        $normalBook = VanillaItems::BOOK();
+        $normalBook = LegacyStringToItemParser::getInstance()->parse(340, 0, 1);
         $normalBook->setCustomName("§r§a§l【 §7THẺ THÔNG THƯỜNG §a】");
         $normalBook->setLore(["§r§fMọi người đều có thể nhận vật phẩm trong thẻ này"]);
 
-        $royalBook = VanillaItems::WRITTEN_BOOK();
+        $royalBook = LegacyStringToItemParser::getInstance()->parse(387, 0, 1);
         $royalBook->setCustomName("§r§a§l【 §6THẺ HUYỀN THOẠI §a】");
         $royalBook->setLore(["§r§6Mua Nó Tại /muassp để nhận các vật phẩm tại thẻ này"]);
 
         $menu->getInventory()->setItem(0, $normalBook);
-        $menu->getInventory()->setItem(1, VanillaBlocks::STAINED_GLASS_PANE()->setColor(DyeColor::LIME)->asItem());
-        $menu->getInventory()->setItem(10, VanillaBlocks::STAINED_GLASS_PANE()->setColor(DyeColor::LIME)->asItem());
+        $menu->getInventory()->setItem(1, LegacyStringToItemParser::getInstance()->parse(160, 5, 1));
+        $menu->getInventory()->setItem(10, LegacyStringToItemParser::getInstance()->parse(160, 5, 1));
         $menu->getInventory()->setItem(27, $royalBook);
-        $menu->getInventory()->setItem(28, VanillaBlocks::STAINED_GLASS_PANE()->setColor(DyeColor::YELLOW)->asItem());
-        $menu->getInventory()->setItem(37, VanillaBlocks::STAINED_GLASS_PANE()->setColor(DyeColor::YELLOW)->asItem());
+        $menu->getInventory()->setItem(28, LegacyStringToItemParser::getInstance()->parse(160, 4, 1));
+        $menu->getInventory()->setItem(37, LegacyStringToItemParser::getInstance()->parse(160, 4, 1));
 
-        $menu->getInventory()->setItem(45, VanillaItems::PAPER()->setCustomName("§r§lĐẾN TRANG BÊN TRÁI"));
-        $menu->getInventory()->setItem(53, VanillaItems::PAPER()->setCustomName("§r§lĐẾN TRANG BÊN PHẢI"));
+        $menu->getInventory()->setItem(45, LegacyStringToItemParser::getInstance()->parse(339, 0, 1)->setCustomName("§r§lĐẾN TRANG BÊN TRÁI"));
+        $menu->getInventory()->setItem(53, LegacyStringToItemParser::getInstance()->parse(339, 0, 1)->setCustomName("§r§lĐẾN TRANG BÊN PHẢI"));
 
-        $menu->getInventory()->setItem(18, VanillaItems::PAPER());
-        $menu->getInventory()->setItem(19, VanillaItems::PAPER());
+        $menu->getInventory()->setItem(18, LegacyStringToItemParser::getInstance()->parse(399, 0, 1));
+        $menu->getInventory()->setItem(19, LegacyStringToItemParser::getInstance()->parse(399, 0, 1));
         $this->menu = $menu;
     }
 }
